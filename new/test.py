19df7fe6008e78130ff5469201ad7b0fcddf0ab4
Tihsrah <harshlf4@gmail.com>
2023-09-15 23:16:56 +0530

    added test.py

--- new/test.py
+++ new/test.py
@@ -1,11 +1,22 @@
-from PIL import ImageGrab
-import numpy as np
-import cv2
+from gym_torcs import TorcsEnv
 
-while True:
-    img = np.array(ImageGrab.grab(bbox=(40, 40, 800, 680)))
-    cv2.imshow('Test Capture', img)
+#### Generate a Torcs environment
+# enable vision input, the action is steering only (1 dim continuous action)
+env = TorcsEnv(vision=True, throttle=False)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+# without vision input, the action is steering and throttle (2 dim continuous action)
+# env = TorcsEnv(vision=False, throttle=True)
+
+ob = env.reset(relaunch=True)  # with torcs relaunch (avoid memory leak bug in torcs)
+# ob = env.reset()  # without torcs relaunch
+
+# Generate an agent
+from sample_agent import Agent
+agent = Agent(1)  # steering only
+action = agent.act(ob, reward, done, vision=True)
+
+# single step
+ob, reward, done, _ = env.step(action)
+
+# shut down torcs
+env.end()
